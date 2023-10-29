d989a6006c0bb77841a4f7ba863a9e41cb5f5481
Nate Marti <nate_marti@sil.org>
2023-10-29 13:44:31 +0100

    refactor to use gui and doc classes

--- src/ScrollSync.py
+++ src/ScrollSync.py
@@ -1,30 +1,175 @@
-# import sys
+import msgbox as util
 import uno
+import unohelper
 
 from com.sun.star.awt import MessageBoxButtons as MSG_BUTTONS
-# from com.sun.star.uno import RuntimeException
+from com.sun.star.awt import XActionListener
+from com.sun.star.awt import ActionEvent
+from com.sun.star.lang import EventObject
+from com.sun.star.ui.dialogs.ExecutableDialogResults import OK, CANCEL
 
 # References:
 #   - UNO API: https://api.libreoffice.org/docs/idl/ref/
 #   - OOO Writer API: https://wiki.openoffice.org/wiki/Writer/API/
 #   - https://www.pitonyak.org/oo.php
-CTX = uno.getComponentContext()
-SM = CTX.getServiceManager()
-DEBUG = False
+#   - https://wiki.documentfoundation.org/Macros/Python_Design_Guide
 
+# _MY_BUTTON = "CommandButton1"
+# _MY_BUTTON2 = "CommandButton2"
+# _MY_LABEL = 'Python listens...'
+# _DLG_PROVIDER = "com.sun.star.awt.DialogProvider"
 
-def get_current_cursor(doc):
-    cursor = doc.CurrentController.getViewCursor()
-    return cursor
+class ActionListener(unohelper.Base, XActionListener):
+    def __init__(self):
+        self.count = 0
+
+    def actionPerformed(self, evt: ActionEvent):
+        self.count += 1
+        if evt.Source.Model.name == _MY_BUTTON:
+            evt.Source.Model.Label = f"{_MY_LABEL} {self.count}"
+        # return
+
+    def disposing(self, evt: EventObject):
+        pass
+
+class ScrollGui():
+    def __init__(self):
+        self.ctx = uno.getComponentContext()
+        self.smgr = self.ctx.ServiceManager
+        self.desktop = self.smgr.createInstanceWithContext('com.sun.star.frame.Desktop', self.ctx)
+        self.dlgprov = self.smgr.createInstanceWithContext('com.sun.star.awt.DialogProvider', self.ctx)
+        dlgpath = 'Standard.DlgScrollSync?location=appliation'
+        # self.dlg = self.dlgprov.createDialog(f"vnd.sun.star.script:{dlgpath}")
+        self.active, self.inactive = self.get_docs()
+
+    def get_docs(self):
+        text_docs = [d for d in self.desktop.Components if d.supportsService('com.sun.star.text.TextDocument')]
+        if len(text_docs) != 2:
+            errbox("There needs to be exactly two Text documents open to run this macro.")
+            return None
+        # TODO: Change "compatible" to mean "same initial paragraph styles IF syncing by heading/paragraph"
+        # # Verify that both docs have the same number and type of significant paragraphs.
+        # if not docs_are_compatible(text_docs):
+        #     msg = f"Docs are not compatible (different number and/or style of paragraphs)."
+        #     errbox(msg)
+        #     return
+        active = ScrollDocument(self.desktop.getCurrentComponent())
+        # TODO: "Inactive" could actually be a list...
+        inactive_doc_index = 1 - self.get_doc_index(active.doc, text_docs)
+        inactive = ScrollDocument(text_docs[inactive_doc_index])
+        return (active, inactive)
+
+    def get_doc_index(self, doc, two_docs):
+        for i in [0, 1]:
+            if doc == two_docs[i]:
+                return i
+        return None
+
+class ScrollDocument():
+    def __init__(self, doc=None):
+        self.doc = doc
+        self.title = self.doc.Title
+        self.view_cursor = self.get_view_cursor()
+        self.scrollbar = self.get_scrollbar()
+        self.scroll_percent = self.get_rel_scrollbar_pos()
+        self.scroll_position = self.get_abs_scrollbar_pos()
+        self.cursor_percent = None
+        self.cursor_position = None
+        self.paragraph_styles = []
+
+    def get_paragraphs(self):
+        paragraphs = []
+        text = self.doc.Text
+        para_enum = text.createEnumeration()
+        while para_enum.hasMoreElements():
+            para = para_enum.nextElement()
+            paragraphs.append(para)
+        return paragraphs
+
+    def get_scrollbar(self):
+        win_ctx = self.doc.CurrentController.Frame.getComponentWindow().getAccessibleContext()
+        ch_ctx = win_ctx.getAccessibleChild(0).getAccessibleContext()
+        vscroll_names = [
+            'Vertical scroll bar',
+            'Barre de défilement verticale',
+        ]
+        for i in range(ch_ctx.getAccessibleChildCount()):
+            c = ch_ctx.getAccessibleChild(i)
+            c_ctx = c.getAccessibleContext()
+            # print(c_ctx.AccessibleName) # TODO: Locale-dependent!
+            if c_ctx.ImplementationName == 'com.sun.star.comp.toolkit.AccessibleScrollBar' and c_ctx.AccessibleName in vscroll_names:
+                return c_ctx
+        return None
+
+    def get_view_cursor(self):
+        cursor = self.doc.CurrentController.getViewCursor()
+        return cursor
+
+    def get_abs_cursor_pos(self):
+        pass
+
+    def set_abs_cursor_pos(self, value):
+        self.view_cursor.gotoRange(value, False)
+
+    def get_rel_cursor_pos(self):
+        # Record current position details.
+        range_cur = self.view_cursor.getStart()
+        pos_cur = self.view_cursor.getPosition()
+        # Move cursor to end of doc to get end position; move back.
+        set_cursor_position(self.view_cursor, self.doc.Text.End)
+        pos_end = self.view_cursor.getPosition()
+        set_cursor_position(self.view_cursor, range_cur)
+        # Calculate.
+        totaly = float(pos_end.Y)
+        currenty = float(pos_cur.Y)
+        relativey = round(currenty / totaly, 2)
+        return relativey
+
+    def set_rel_cursor_pos(self, value):
+        pass
+
+    def get_abs_scrollbar_pos(self):
+        return self.scrollbar.CurrentValue
+
+    def set_abs_scrollbar_pos(self, value):
+        self.scrollbar.setCurrentValue(int(value))
+
+    def get_rel_scrollbar_pos(self):
+        totaly = float(self.scrollbar.MaximumValue)
+        currenty = float(self.scrollbar.CurrentValue)
+        relativey = round(currenty / totaly, 2)
+        return relativey
+
+    def set_rel_scrollbar_pos(self, value):
+        totaly = float(self.scrollbar.MaximumValue)
+        absy = int(float(value) * totaly)
+        self.set_abs_scrollbar_pos(absy)
+
+def MsgBox(txt: str):
+    mb = util.MsgBox(uno.getComponentContext())
+    mb.addButton("OK")
+    mb.show(txt, 0, "Python")
+
+def createUnoDialog(libr_dlg: str, embedded=False):
+    ctx = XSCRIPTCONTEXT.getComponentContext()
+    smgr = ctx.ServiceManager
+    if embedded:
+        model = XSCRIPTCONTEXT.getDocument()
+        dp = smgr.createInstanceWithArguments(_DLG_PROVIDER, (model,))
+        # dp = SM.createInstanceWithArguments(_DLG_PROVIDER, (model,))
+        location = '?location=document'
+    else:
+        dp = smgr.createInstanceWithContext(_DLG_PROVIDER, ctx)
+        print(type(dp))
+        location = '?location=application'
+    dlg = dp.createDialog(f"vnd.sun.star.script:{libr_dlg}{location}")
+    return dlg
 
 def get_cursor_range_start(cursor):
     return cursor.Start
 
 def set_cursor_position(cursor, range_obj):
     cursor.gotoRange(range_obj, False)
-
-def get_desktop():
-    return SM.createInstanceWithContext('com.sun.star.frame.Desktop', CTX)
 
 def get_paragraph_index(doc, paragraph):
     # TODO: This assumes each paragraph has unique content!
@@ -34,15 +179,6 @@ def get_paragraph_index(doc, paragraph):
             return i
     return None
 
-def get_paragraphs(doc):
-    paragraphs = []
-    text = doc.Text
-    para_enum = text.createEnumeration()
-    while para_enum.hasMoreElements():
-        para = para_enum.nextElement()
-        paragraphs.append(para)
-    return paragraphs
-
 def msgbox(message, title='LibreOffice', buttons=MSG_BUTTONS.BUTTONS_OK, type_msg='infobox'):
     """ Create message box
         type_msg: infobox, warningbox, errorbox, querybox, messbox
@@ -51,8 +187,7 @@ def msgbox(message, title='LibreOffice', buttons=MSG_BUTTONS.BUTTONS_OK, type_ms
     toolkit = create_instance('com.sun.star.awt.Toolkit')
     parent = toolkit.getDesktopWindow()
     box = toolkit.createMessageBox(parent, type_msg, buttons, title, str(message))
-    box.execute()
-    # return mb.execute()
+    return mb.execute()
 
 def errbox(message, title='Sync Cursors', buttons=MSG_BUTTONS.BUTTONS_OK, type_msg='errorbox'):
     toolkit = create_instance('com.sun.star.awt.Toolkit')
@@ -67,12 +202,6 @@ def create_instance(name, with_context=False):
         instance = SM.createInstance(name)
     return instance
 
-def get_two_docs(desktop):
-    text_docs = [d for d in desktop.Components if d.supportsService('com.sun.star.text.TextDocument')]
-    if len(text_docs) != 2:
-        errbox("There needs to be exactly two Text documents open to run this macro.")
-    return text_docs
-
 def docs_are_compatible(two_docs):
     paragraph_styles = dict()
     for d in two_docs:
@@ -86,13 +215,6 @@ def docs_are_compatible(two_docs):
     doc_titles = list(paragraph_styles.keys())
     return paragraph_styles.get(doc_titles[0]) == paragraph_styles.get(doc_titles[1])
 
-def get_active_doc_index(desktop, two_docs):
-    active_doc = desktop.getCurrentComponent()
-    for i in [0, 1]:
-        if active_doc == two_docs[i]:
-            return i
-    return None
-
 def get_scroll_action(i_initial, i_final):
     action = None
     diff = i_initial - i_final
@@ -101,21 +223,6 @@ def get_scroll_action(i_initial, i_final):
     elif diff < 0: # need to move the view down the page
         action = 1 # line-down, 3 = screen-down,
     return action
-
-def get_vscrollbar_context(doc):
-    win_ctx = doc.CurrentController.Frame.getComponentWindow().getAccessibleContext()
-    ch_ctx = win_ctx.getAccessibleChild(0).getAccessibleContext()
-    vscroll_names = [
-        'Vertical scroll bar',
-        'Barre de défilement verticale',
-    ]
-    for i in range(ch_ctx.getAccessibleChildCount()):
-        c = ch_ctx.getAccessibleChild(i)
-        c_ctx = c.getAccessibleContext()
-        # print(c_ctx.AccessibleName) # TODO: Locale-dependent!
-        if c_ctx.ImplementationName == 'com.sun.star.comp.toolkit.AccessibleScrollBar' and c_ctx.AccessibleName in vscroll_names:
-            return c_ctx
-    return None
 
 def get_relative_cursor_yposition(doc, cursor):
     # Record current position details.
@@ -128,12 +235,6 @@ def get_relative_cursor_yposition(doc, cursor):
     # Calculate.
     totaly = float(pos_end.Y)
     currenty = float(pos_cur.Y)
-    relativey = round(currenty / totaly, 2)
-    return relativey
-
-def get_relative_scroll_yposition(vscrollbar):
-    totaly = float(vscrollbar.MaximumValue)
-    currenty = float(vscrollbar.CurrentValue)
     relativey = round(currenty / totaly, 2)
     return relativey
 
@@ -158,16 +259,36 @@ def scroll_to_inactive_cursor_location(doc_active, ipara_active, doc_inactive, i
     if ct == max_scroll_lines:
         msgbox(f"Warning: Stopped early. The window has scrolled the maximum number of allowed lines: {max_scroll_lines}.")
 
-def scroll_to_active_scrollbar_value(vsb_active, vsb_inactive):
-    if vsb_active.CurrentValue != vsb_inactive.CurrentValue:
-        vsb_inactive.setCurrentValue(vsb_active.CurrentValue)
+def dialogTest(*args):
+    # dp = SM.createInstanceWithContext(_DLG_PROVIDER, CTX)
+    dp = create_instance(_DLG_PROVIDER, with_context=True)
+    libr_dlg = "Standard.DlgScrollSync"
+    location = '?location=application'
+    ui = dp.createDialog(f"vnd.sun.star.script:{libr_dlg}{location}")
+    ui.Title = "Python X[any]Listener"
+    b1Ctl = ui.getControl(_MY_BUTTON)
+    b2Ctl = ui.getControl(_MY_BUTTON2)
+    act = ActionListener()
+    b1Ctl.addActionListener(act)
+    b2Ctl.addActionListener(act)
+    rc = ui.execute()
+    if rc == OK:
+        MsgBox("User clicked 'OK'")
+    elif rc == CANCEL:
+        MsgBox("User clicked 'Cancel'")
+    ui.dispose()
+    b1Ctl.removeActionListener(act)
+    b2Ctl.removeActionListener(act)
 
-def update_inactive_doc_cursor_position():
-    desktop = get_desktop()
+def updateInactiveDocCursorPosition():
+    desktop = get_desktop() # REMOVE
     # Note: Doc indexing seems to be in reverse order of opening; i.e. last opened is i=0.
     two_docs = get_two_docs(desktop)
+    if two_docs is None:
+        return
     # Verify that both docs have the same number and type of significant paragraphs.
     if not docs_are_compatible(two_docs):
+        # TODO: Change "compatible" to mean "same initial paragraph styles"
         msg = f"Docs are not compatible (different number and/or style of paragraphs)."
         errbox(msg)
         return
@@ -191,3 +312,27 @@ def update_inactive_doc_cursor_position():
     # Scroll window to put cursor at top.
     # scroll_to_inactive_cursor_location(doc_active, ipara_active, doc_inactive, ipara_inactive, cur_inactive, vsb_inactive)
     scroll_to_active_scrollbar_value(vsb_active, vsb_inactive)
+
+def updateByScrollbarPercentage():
+    app = ScrollGui()
+    app.inactive.set_rel_scrollbar_pos(app.active.get_rel_scrollbar_pos())
+
+def updateByScrollbarValue():
+    app = ScrollGui()
+    app.inactive.set_abs_scrollbar_pos(app.active.scroll_position)
+
+def updateByHeadingPosition():
+    app = ScrollGui()
+    # TODO.
+
+def updateByParagraphPosition():
+    app = ScrollGui()
+    # TODO.
+
+g_exportedScripts = (
+    updateByScrollbarPercentage,
+    updateByScrollbarValue,
+    # updateByHeadingPosition,
+    # updateByParagraphPosition,
+    dialogTest,
+)
